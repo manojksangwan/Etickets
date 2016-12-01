60144b173c380362224f63a6524ec1c616f10443
manojK <manojksangwan@gmail.com>
2016-12-01 17:03:20 +0530

    from Office

--- app/src/main/java/in/gov/hartrans/etickets/TestPayment.java
+++ app/src/main/java/in/gov/hartrans/etickets/TestPayment.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
 
@@ -43,13 +44,13 @@ public class TestPayment extends AppCompatActivity {
         Bundle bundle = new Bundle();
         bundle.putString("mid", "HDFC000000000044");
         bundle.putString("merchantKey", "7c448fb4c6eb425e86f8817ae5488533");
-        bundle.putString("merchantTxnID", "123456789013");
-        bundle.putString("transactionDesc", "pnr123456789");
+        bundle.putString("merchantTxnID", "123456789143");
+        bundle.putString("transactionDesc", "pnr123456143");
         bundle.putString("currency", "INR");
-        bundle.putString("appName", "in.gov.hartrans.etickets");
-        bundle.putString("paymentType", "p2m");
-        bundle.putString("transactionType", "PAY");
-        bundle.putString("payeePayAddress", "Director General State Transport Haryana");
+        bundle.putString("appName", "in.gov.hartrans.testupi");
+        bundle.putString("paymentType", "P2M");
+        bundle.putString("transactionType", "COLLECT");
+        bundle.putString("payeePayAddress", "");
         bundle.putString("payeeAccntNo", "");
         bundle.putString("payeeIFSC", "");
         bundle.putString("payeeAadhaarNo", "");
@@ -66,7 +67,7 @@ public class TestPayment extends AppCompatActivity {
         bundle.putString("payerMMID", "");
         bundle.putString("payeeMMID", "");
         bundle.putString("refurl", "");
-        bundle.putString("amount","1122.00");
+        bundle.putString("amount","143.00");
         bundle.putString("add1", "");
         bundle.putString("add2", "");
         bundle.putString("add3", "");
@@ -87,36 +88,48 @@ public class TestPayment extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         try{
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null)
-        {
-            Bundle bundle = getIntent().getExtras();
-/*            String pgMeTrnRefNo = bundle.getString("pgMeTrnRefNo");
-            String orderNo = bundle.getString("orderNo");
-            String txnAmount = bundle.getString("txnAmount");
-            String tranAuthdate = bundle.getString("tranAuthdate");
-            String approvalCode = bundle.getString("approvalCode");
-            String payerVA = bundle.getString("payerVA");
-            String npciTxnId = bundle.getString("npciTxnId");
-            String refId = bundle.getString("refId");
-            String add1 = bundle.getString("add1");
-            String add2 = bundle.getString("add2");
-            String add3 = bundle.getString("add3");
-            String add4 = bundle.getString("add4");
-            String add5 = bundle.getString("add5");
-            String add6 = bundle.getString("add6");
-            String add7 = bundle.getString("add7");
-            String add8 = bundle.getString("add8");
-            String add9 = bundle.getString("add9");
-            String add10 = bundle.getString("add10");
-            String statusCode = bundle.getString("status");
-            String statusDesc = bundle.getString("statusDesc");
-            String responsecode = bundle.getString("responsecode");
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null)
+            {
+                Bundle bundle = data.getExtras();
+                String pgMeTrnRefNo = bundle.getString("pgMeTrnRefNo");
+                String orderNo = bundle.getString("orderNo");
+                String txnAmount = bundle.getString("txnAmount");
+                String tranAuthdate = bundle.getString("tranAuthdate");
+                String approvalCode = bundle.getString("approvalCode");
+                String payerVA = bundle.getString("payerVA");
+                String npciTxnId = bundle.getString("npciTxnId");
+                String refId = bundle.getString("refId");
+                String add1 = bundle.getString("add1");
+                String add2 = bundle.getString("add2");
+                String add3 = bundle.getString("add3");
+                String add4 = bundle.getString("add4");
+                String add5 = bundle.getString("add5");
+                String add6 = bundle.getString("add6");
+                String add7 = bundle.getString("add7");
+                String add8 = bundle.getString("add8");
+                String add9 = bundle.getString("add9");
+                String add10 = bundle.getString("add10");
+                String statusCode = bundle.getString("status");
+                String statusDesc = bundle.getString("statusDesc");
+                String responsecode = bundle.getString("responsecode");
 
-            outputView.setText(statusCode + "  " + responsecode);
-            */
-        }}catch (Exception ex)
-        {
+                String responsE;
+                responsE="States Code: "  +statusCode + "<br/>";
+                responsE+="States Desc: "  +statusDesc + "<br/>";
+                responsE+="responsecode: "  +responsecode + "<br/>";
+                responsE+="pgMeTrnRefNo: "  +pgMeTrnRefNo + "<br/>";
+                responsE+="orderNo: "  +orderNo + "<br/>";
+                responsE+="txnAmount: "  +txnAmount + "<br/>";
+                responsE+="tranAuthdate: "  +tranAuthdate + "<br/>";
+                responsE+="approvalCode: "  +approvalCode + "<br/>";
+                responsE+="payerVA: "  +payerVA + "<br/>";
+                responsE+="npciTxnId: "  +npciTxnId + "<br/>";
+                responsE+="refId: "  +refId + "<br/>";
+
+                outputView.setText(Html.fromHtml(responsE));
+
+            }}catch (Exception ex) {
             outputView.setText(ex.getMessage());
         }
     }
